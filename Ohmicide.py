ba7c1478fbecd5bdecbb0d434bd50903ddee42b0
Will Marshall <will@willmarshall.me>
2012-05-05 17:37:41 +1000

    First commit:

---
+++ Ohmicide.py
@@ -0,0 +1,64 @@
+import Live
+
+
+from Constants import *
+from _Framework.ControlSurface import ControlSurface
+from _Framework.InputControlElement import *
+from _Framework.SliderElement import SliderElement
+from _Framework.ButtonElement import ButtonElement
+from _Framework.EncoderElement import EncoderElement
+from _Framework.ButtonMatrixElement import ButtonMatrixElement
+from _Framework.MixerComponent import MixerComponent
+from _Framework.ClipSlotComponent import ClipSlotComponent
+from _Framework.ChannelStripComponent import ChannelStripComponent
+from _Framework.SceneComponent import SceneComponent
+from _Framework.SessionZoomingComponent import SessionZoomingComponent
+from _Framework.ChannelTranslationSelector import ChannelTranslationSelector
+
+from _Livid_Framework.LividMixerComponent import LividMixerComponent
+from _Livid_Framework.LividSessionComponent import LividSessionComponent
+from _Livid_Framework.LividTransportComponent import LividTransportComponent
+from _Livid_Framework.LividSessionZoomingComponent import LividSessionZoomingComponent
+
+class Ohmicide(ControlSurface):
+  __module__ = __name__
+  __doc__ = " Ohmicide controller script "
+
+  def __init__(self, c_instance):
+    ControlSurface.__init__(self, c_instance)
+    self.set_suppress_rebuild_requests(True) # Turn off rebuild MIDI map until after we're done setting up
+
+    # Configure each of the primarily elements
+    self.setup_mixer()
+    self.setup_session()
+    self.setup_transport()
+
+    self.set_suppress_rebuild_requests(False) #Turn rebuild back on, now that we're done setting up
+
+  def setup_mixer(self):
+    self.mixer = LividMixerComponent(faders = FADERS, sends = SENDS, 
+        crossfader = CROSSFADER, 
+        master = MASTER, 
+        cue = CUE_VOLUME,
+        solos = SOLOS,
+        selects = TRACK_SELECTS, 
+        master_select = MASTER_SELECT, 
+        arms = ARMS,
+        channel = CHANNEL)
+        #mutes = MUTES)
+  
+  def setup_session(self):
+    self.session = LividSessionComponent(matrix = MATRIX, 
+        navigation = NAVIGATION_BUTTONS, 
+        scene_launches = SCENE_LAUNCH, 
+        stops = STOPS, 
+        stop_all = STOP_ALL, 
+        channel = CHANNEL,
+        mixer = self.mixer)
+    self.session_zoom = LividSessionZoomingComponent(session = self.session, shift = SHIFT, channel = CHANNEL)
+    
+    # Session zoom
+
+  def setup_transport(self):
+    self.transport = LividTransportComponent(play = PLAY, stop = STOP, bpm_up = BPM_UP, bpm_down = BPM_DOWN, channel = CHANNEL)
+    
